626fbf062088af05bcf755bd5714f08c4c1947cf
honeybee <shelby.e.pagan@gmail.com>
2023-10-30 12:39:54 -0400

    added submit/restart buttons. Added stats display

--- app.js
+++ app.js
@@ -1,62 +1,66 @@
-console.log('js:loaded')
+console.log("js:loaded");
 
 //Master Array to store all game board button id's//
 const buttons = document.querySelectorAll(".grid-button");
-const masterSequence = []
-buttons.forEach(button => {
-    masterSequence.push(button.id);
-    button.addEventListener("click", () => handleButtonClick(button.id));
+const masterSequence = [];
+buttons.forEach((button) => {
+  masterSequence.push(button.id);
+  button.addEventListener("click", () => handleButtonClick(button.id));
 });
 
 //Computer empty array and Player empty arrray//
-const computerSequence = []
-const playerSequence = []
+const computerSequence = [];
+const playerSequence = [];
 
 //console.log(computerSequence);
 //console.log(playerSequence);
 
 //Randomize the game array//
 
-function generateSequence(){
-    masterSequence.sort(() => Math.random() - 0.5);
-    computerSequence.push(...masterSequence.slice(0, 4));
+function generateSequence() {
+  masterSequence.sort(() => Math.random() - 0.5);
+  computerSequence.push(...masterSequence.slice(0, 4));
 }
 
-generateSequence()
+generateSequence();
 console.log(computerSequence);
 
 //Event listener for start button
 const startButton = document.getElementById("start-button");
 startButton.addEventListener("click", startGame);
 
-//Start game function 
+//Start game function
 
 function startGame() {
-    alert ("Watch the pattern carefully!")
-    displaySequence();
+  alert("Watch the pattern carefully!");
+  displaySequence();
 }
-
 
 //Display randomized pattern on game board. Using async/await to make buttons shake one after another, instead of all at the same time
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function displaySequence() {
-    for (const id of computerSequence) {
-        const button = document.getElementById(id);
-        button.classList.add("shake");
-        await sleep(1000);
-        button.classList.remove("shake");
-        await sleep(1000);
-    }
-    alert ("Repeat the pattern and press submit")
+  for (const id of computerSequence) {
+    const button = document.getElementById(id);
+    button.classList.add("shake");
+    await sleep(1000);
+    button.classList.remove("shake");
+    await sleep(1000);
+  }
+  alert("Repeat the pattern and press submit");
 }
 
 //Event listener for player clicking grid-buttons(placed above in buttons.forEach)
 //Function that adds clicked buttons to playerSequence array
 
 function handleButtonClick(clickedId) {
-    playerSequence.push(clickedId);
-    console.log("Button " + clickedId + " was clicked. playerSequence: " + playerSequence.join(", "));
+  playerSequence.push(clickedId);
+  console.log(
+    "Button " +
+      clickedId +
+      " was clicked. playerSequence: " +
+      playerSequence.join(", ")
+  );
 }
